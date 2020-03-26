33615ecd45cc74b569e18bbddb67c6a3b169f2fb
Segun Ayobamidele <ayobamidelesegundaniel@gmail.com>
2020-03-26 15:37:48 +0100

    update upto 26/03/2020

---
+++ script.js
@@ -0,0 +1,14 @@
+var name = "I am Segun";
+var Courses = ["HTML", "CSS", "JavaScript", "Python", "PHP", "Csharp"];
+
+console.log(name)
+console.log(Courses)
+
+let number = Courses.length;
+
+console.log("number of courses is " + number)
+
+
+for (let i = 2; i < 200; i = i + 2) {
+    console.log(i)
+}
